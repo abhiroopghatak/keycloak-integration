5d790d39c0cb4c45134526c24b3a27f3b1652f84
abhiroopghatak <40733362+abhiroopghatak@users.noreply.github.com>
2021-02-15 18:30:04 +0530

    KeycloakIntegrationApplication.java
    KeycloakIntegrationApplication.java

---
+++ src/main/java/com/abhiroop/integration/keycloakintegration/KeycloakIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.abhiroop.integration.keycloakintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakIntegrationApplication.class, args);
+		System.err.println("KeycloakIntegrationApplication started.");
+	}
+
+}
